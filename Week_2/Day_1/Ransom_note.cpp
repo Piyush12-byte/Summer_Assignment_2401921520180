6a6f4d429ca52942533adcfe7e7a4b3918169f8d
Piyush12-byte <piyushkatiyar69@gmail.com>
2026-06-08 20:16:35 +0530

    Create Ransom_note.cpp

---
+++ Week_2/Day_1/Ransom_note.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        
+        int n=ransomNote.size();
+        int m=magazine.size();
+
+        if(n>m) return false;
+
+        unordered_map<char,int>mp;
+
+        for(char ch:magazine){
+            mp[ch]++;
+        }
+
+        for(char ch:ransomNote){
+            if(mp[ch]==0){
+                return false;
+            }
+           mp[ch]--; 
+        }
+
+      return true;
+    }
+};
