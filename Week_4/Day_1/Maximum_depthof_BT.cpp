c6bb536010a3a526f100923680b3f335baacc84c
Piyush12-byte <piyushkatiyar69@gmail.com>
2026-06-22 11:22:19 +0530

    Create Maximum_depthof_BT.cpp

---
+++ Week_4/Day_1/Maximum_depthof_BT.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        
+        if(root==NULL){
+            return 0;
+        }
+
+        int h1=maxDepth(root->left);
+        int h2=maxDepth(root->right);
+
+        int ans=max(h1,h2)+1;
+
+        return ans;
+    }
+};
